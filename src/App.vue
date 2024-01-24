9fd844ad6a433437f58962ef7625a691dd810517
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-01-24 16:07:41 -0700

    first

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="contenedor">
+    <h1>Juego 3 en Raya</h1>
+    <div class="fila" v-for="(row, rowIndex) in tablero" :key="rowIndex">
+      <CasillaComponent 
+        v-for="(column, columnIndex) in row" :key="columnIndex"
+        :figura="column"
+        @click="cambiaEstadoCasilla(rowIndex, columnIndex)" />
+    </div>
+    <div class="fila">
+      <button @click="reset">Reset</button>
+      <button @click="turno = FIGURA.X">{{ FIGURA.X }}</button>
+      <button @click="turno = FIGURA.O">{{ FIGURA.O }}</button>
+    </div>
+  </div>  
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+import { ref } from "vue";
+import CasillaComponent from "./assets/CasillaComponent.vue";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+enum FIGURA {
+  X = "❌",
+  O = "🌀"
+}
+
+const turno = ref<FIGURA>(FIGURA.X);
+
+const tablero = ref<string[][]>([
+  [ "", "", "" ],
+  [ "", "", "" ],
+  [ "", "", "" ]
+]);
+
+const reset = () => {
+  tablero.value = [
+    [ "", "", "" ],
+    [ "", "", "" ],
+    [ "", "", "" ]
+  ];
+}
+
+const cambiaEstadoCasilla = (fila: number, columna: number) => {
+  if (!tablero.value[fila][columna]) {
+    tablero.value[fila][columna] = turno.value;
+    turno.value = turno.value === FIGURA.X ? FIGURA.O : FIGURA.X;
   }
-});
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  background-color: #1a1a1a;
+}
+
+h1 {
+  color: white;
+}
+
+.contenedor {
+  display: flex;
+  width: 100%;
+  height: 90vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.fila {
+  width: 350px;
+  display: flex;
 }
 </style>
---
+++ src/assets/CasillaComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <button>{{ figura }}</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps({
+    figura: String
+});
+</script>
+
+<style scoped>
+button {
+    width: 100px;
+    height: 100px;
+    font-size: 50px;
+    background-color: transparent;
+    border-color: #42b883;
+    margin: 5px;
+}
+</style>
